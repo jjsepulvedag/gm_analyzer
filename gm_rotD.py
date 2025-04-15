9b126149d622cee94f3e3fd9d6b6a30d91d66b84
JJSG - UC <juanjose.sepulvedagarcia@pg.canterbury.ac.nz>
2025-04-15 19:31:51 +1200

    Minor change to the file gm_rotD.py. I recommended to use partial functions to use this function

--- gm_rotD.py
+++ gm_rotD.py
@@ -1,6 +1,31 @@
+r"""FILE TITLE
+
+PURPOSE: 
+# ------------------------------------- . ------------------------------------ #
+FUNCTIONS:
+funct1    : short description
+# ------------------------------------- . ------------------------------------ #
+AUTHORSHIP
+Written by: Juan Jose Sepulveda Garcia
+Mail      : jjs134@uclive.ac.nz / jjsepulvedag@unal.edu.co
+Date      : March 2025
+# ------------------------------------- . ------------------------------------ #
+REFERENCES:
+NA
+
+IMPORTANT NOTES
+Most of these functions require the IMs function as an argument. For most IMs 
+this is easy. However, for other IMS (such as pSA) we have to use either 
+partial functions (from functools import partial) or lambda function to set 
+the parameters inside the IM function and only let the waveform varying. 
+"""
+
+
+
 import numpy as np
 
-def rotD50():
+
+def rotD50(gm000, gm090, dd, im):
 
     return None
 
