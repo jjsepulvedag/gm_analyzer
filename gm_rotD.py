232a9ff695c1a398fa9ae4939d4d2ba634e6bd53
JJSG - UC <juanjose.sepulvedagarcia@pg.canterbury.ac.nz>
2025-04-22 18:06:59 +1200

    gm_rotD.rod50 written today

--- gm_rotD.py
+++ gm_rotD.py
@@ -22,20 +22,45 @@ the parameters inside the IM function and only let the waveform varying.
 
 import numpy as np
 
+def rotateGM(gm000, gm090, theta):
 
-def rotD50(gm000, gm090, N, imFunc):
+    gm_r = gm000*np.cos(theta) + gm090*np.sin(theta) # rotated ground motion
+
+    return gm_r
+
+
+
+def rotD50(gm000, gm090, N, imFunc, nIM):
     '''
-    Note: 
-        Please include a N equal or greater than 2. For a dStep of 5°, use N=19
+    INPUT: 
+        - gm000: ground-motion record, component 000 (north) 
+        - gm090: ground-motion record, component 090 (east) 
+        - N: number of elements for the computation of rotd50 (rotations)
+        - imFunc: intensity-measure function
+        - nIm: number of elements returned by inFunc. For most IMs, this value 
+        is equal to 1, for pSA this value depends on the number of periods
+    NOTE: 
+        PLEASE INCLUDE A N EQUAL OR GREATER THAN 2. 
+        - For a dStep of 01°, use N=181
+        - For a dStep of 02°, use N=91
+        - For a dStep of 05°, use N=37 
+        - For a dStep of 10°, use N=19
+        - For a dStep of 15°, use N=13
+        - For a dStep of 20°, use N=10
+        - For a dStep of 30°, use N=07
+        - For a dStep of 45°, use N=05
     '''
 
-    theta = np.linspace(0, 90, N)
+    thetas = np.radians(np.linspace(0, 180, N))
+    ims = np.zeros((thetas.shape[0], nIM))
 
+    for i in range(thetas.shape[0]):
+        gm_r = rotateGM(gm000, gm090, thetas[i]) # get rotated ground motion
+        ims[i, :] = imFunc(gm_r)
     
-
-
-
-    return None
+    imRotd50 = np.median(ims, axis=0) 
+    
+    return imRotd50
 
 def rotD100():
 
