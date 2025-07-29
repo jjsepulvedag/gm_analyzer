a0a57d96794363bcda5ed42751ce30641567e75e
JJSG - UC <juanjose.sepulvedagarcia@pg.canterbury.ac.nz>
2025-07-30 11:52:52 +1200

    Minor modifcation to the name of the function g_to_cm2 and related functions in the file gm_misc.py

--- gm_misc.py
+++ gm_misc.py
@@ -1,16 +1,20 @@
 import numpy as np
 from scipy.integrate import cumtrapz
 
-def conv_M02Mw():
+def Mw_to_M0():
     
     return None
 
-def g_2_cms2(gm_record):
+def M0_to_Mw():
+    
+    return None
+
+def g_to_cms2(gm_record):
     # Converts units of a ground-motion record -> Gravity to cm/s2
     gm_cms2 = gm_record*981
     return gm_cms2
 
-def cms2_2_g(gm_record):
+def cms2_to_g(gm_record):
     # Converts units of a ground-motion rec ord -> cm/s2 to gravity
     gm_gravity = gm_record/981
     return gm_gravity
